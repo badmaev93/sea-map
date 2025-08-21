96924634749bb120bc999d254e4ec9672c2f6126
badmaev93 <82601790+badmaev93@users.noreply.github.com>
2025-08-21 12:48:55 +1000

    Add files via upload

---
+++ sea-api-backend/server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const cors = require('cors');
+const fs = require('fs'); 
+const csv = require('csv-parser'); 
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+
+app.get('/api/data', (req, res) => {
+    const results = [];
+
+    fs.createReadStream('data.csv')
+      .on('error', (error) => {
+ъ        console.error("Ошибка чтения файла:", error);
+        res.status(500).json({ error: 'Не удалось прочитать файл с данными' });
+      })
+      .pipe(csv()) 
+      .on('data', (data) => {
+        results.push(data);
+      })
+      .on('end', () => {
+        
+        const processedResults = results.map(row => ({
+            ...row, 
+            depth_m: parseFloat(row.depth_m),
+            temp_c: parseFloat(row.temp_c),
+            salinity_psu: parseFloat(row.salinity_psu),
+            oxygen_mgl: parseFloat(row.oxygen_mgl),
+            ph: parseFloat(row.ph),
+            latitude: parseFloat(row.latitude),
+            longitude: parseFloat(row.longitude)
+        }));
+        
+        res.json(processedResults);
+      });
+});
+
+app.listen(port, () => {
+    console.log(`Сервер запущен на порту ${port}`);
+});
